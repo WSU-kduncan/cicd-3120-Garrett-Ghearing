babb9d433a05c677852639be9cf5311d2c7f9a53
Garrett Ghearing <ghearing.3@wright.edu>
2022-12-01 17:55:37 -0500

    adding the needed files for deployment

---
+++ refresh.sh
@@ -0,0 +1,7 @@
+# stop old
+docker stop site
+docker ps -a
+docker rm site
+docker ps -a
+docker pull ghearinggarrett/garrettproject5
+docker run -d -p 8070:80 --name site ghearinggarrett/garrettproject5:firsttry
