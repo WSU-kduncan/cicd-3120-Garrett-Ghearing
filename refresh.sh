658fdeda158a92fa6a6b821f2c67a49c2324b738
Garrett-Ghearing <70773439+Garrett-Ghearing@users.noreply.github.com>
2022-12-02 14:18:06 -0500

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -4,4 +4,4 @@ docker ps -a
 docker rm site
 docker ps -a
 docker pull ghearinggarrett/garrettproject5
-docker run -d -p 8070:80 --name site ghearinggarrett/garrettproject5:firsttry
+docker run -d -p 8080:80 --name site ghearinggarrett/garrettproject5:firsttry
